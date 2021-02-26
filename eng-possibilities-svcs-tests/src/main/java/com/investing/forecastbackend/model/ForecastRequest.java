ebfcfcf2350fe09112e257c1602f826e744696d8
bel11 <67809754+bel11@users.noreply.github.com>
2021-02-26 15:13:16 -0500

    Update ForecastRequest.java

--- eng-possibilities-svcs-tests/src/main/java/com/investing/forecastbackend/model/ForecastRequest.java
+++ eng-possibilities-svcs-tests/src/main/java/com/investing/forecastbackend/model/ForecastRequest.java
@@ -1,16 +1,24 @@
 package com.investing.forecastbackend.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+//import lombok.Data;
+//import lombok.Getter;
+//import lombok.Setter;
 
 import java.util.Map;
 
 // TODO Model for the request for investment forecast
-@Data
-@Getter
-@Setter
+//@Data
+//@Getter
+//@Setter
 public class ForecastRequest {
     private Map<String, Double> request;
+
+    public Map<String, Double> getRequest() {
+        return request;
+    }
+
+    public void setRequest(Map<String, Double> request) {
+        this.request = request;
+    }
 
 }
