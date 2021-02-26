824778d56337f3e1e6cc5dfa9a216cf760e3f51b
bel11 <67809754+bel11@users.noreply.github.com>
2021-02-26 15:13:49 -0500

    Update ForecastResponse.java

--- eng-possibilities-svcs-tests/src/main/java/com/investing/forecastbackend/model/ForecastResponse.java
+++ eng-possibilities-svcs-tests/src/main/java/com/investing/forecastbackend/model/ForecastResponse.java
@@ -1,15 +1,23 @@
 package com.investing.forecastbackend.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+//import lombok.Data;
+//import lombok.Getter;
+//import lombok.Setter;
 
 import java.util.List;
 
 // TODO Model for the response for investment forecast
-@Data
-@Getter
-@Setter
+//@Data
+//@Getter
+//@Setter
 public class ForecastResponse {
     private List<Double> response;
+
+    public List<Double> getResponse() {
+        return response;
+    }
+
+    public void setResponse(List<Double> response) {
+        this.response = response;
+    }
 }
