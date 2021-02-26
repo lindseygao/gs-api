81e3cd56145034bdb0cf5c78a079ae0ae1a4efb3
bel11 <67809754+bel11@users.noreply.github.com>
2021-02-26 15:14:13 -0500

    Update InvestmentDetail.java

--- eng-possibilities-svcs-tests/src/main/java/com/investing/forecastbackend/model/InvestmentDetail.java
+++ eng-possibilities-svcs-tests/src/main/java/com/investing/forecastbackend/model/InvestmentDetail.java
@@ -1,17 +1,30 @@
 package com.investing.forecastbackend.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+//import lombok.Data;
+//import lombok.Getter;
+//import lombok.Setter;
 
 import java.util.List;
 
-@Data
-@Getter
-@Setter
+//@Data
+//@Getter
+//@Setter
 public class InvestmentDetail {
     private String category;
     private String minimum;
     private List<String> data;
+    
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public List<String> getData() {
+        return data;
+    }
+    public void setData(List<String> data) {
+        this.data = data;
+    }
 }
 
